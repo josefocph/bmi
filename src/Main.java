fdc8ae0587b016b46a970ffe779ef71b561b0a3e
Josefo Koirala <josefokoirala@gmail.com>
2026-09-21 09:44:56 +0200

    Added Dorthe to the list of persons

--- src/Main.java
+++ src/Main.java
@@ -2,10 +2,12 @@ void main() {
     double bmiAlex = calculateBmi(82.0, 1.79);
     double bmiBodil = calculateBmi(76, 1.90);
     double bmiCharles = calculateBmi(58, 1.70);
+    double bmiDorthe = calculateBmi(55, 1.65);
 
     IO.println("Alex, dit BMI er " + bmiAlex + ". Du er " + classifyBmi(bmiAlex));
     IO.println("Bodil, dit BMI er " + bmiBodil + ". Du er " + classifyBmi(bmiBodil));
     IO.println("Charles dit BMI er" + bmiCharles + ". Du er " + classifyBmi(bmiCharles));
+    IO.println("Dorthe dit BMI er" + bmiDorthe + ". Du er " + classifyBmi(bmiDorthe));
 }
 
 double calculateBmi(double weight, double height) {
